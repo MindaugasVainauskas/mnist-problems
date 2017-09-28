1a3f50c13489512cc59b3de89f1decc61732ba64
M.Vainauskas <mindelis1@gmail.com>
2017-09-28 13:35:26 +0100

    Added def for function to read label sets

--- mnist-import-gz.py
+++ mnist-import-gz.py
@@ -3,18 +3,40 @@
 
 #import gzip library
 import gzip
+import struct
 
 #dataPath = 'data/train-labels-idx1-ubyte.gz'
 dataPath = 'data/train-images-idx3-ubyte.gz'
 
-f = gzip.open(dataPath, 'rb')
-bytes = f.read(4)
-print(bytes)
-#Adapted from:
-#	https://docs.python.org/3/library/stdtypes.html#int.from_bytes
-x = int.from_bytes(bytes, byteorder = 'big')
-print("Big endian decoded value: %d" % (x))
+def read_labels_from_file(fPath):
+	with gzip.open(fPath, 'rb') as f:
+		mNum= f.read(4)
+		mNum = int.from_bytes(mNum, 'big')
+		print("Magic number: ", mNum)
+		
+		nolab = f.read(4)
+		nolab = int.from_bytes(nolab, 'big')
+		print("Label amount: ", nolab)
+		
+		labels = [f.read(1) for i in range(nolab)]
+		labels = [int.from_bytes(label, 'big') for label in labels]
+		
+		return labels
+	
+train_labels = read_labels_from_file('data/train-labels-idx1-ubyte.gz')
+test_labels = read_labels_from_file('data/test-labels-idx3-ubyte.gz')
 
-y = int.from_bytes(bytes, byteorder = 'little')
-print("Little endian decoded value: %d" % (y))
-f.close()
+# f.close()
+# f = gzip.open(dataPath, 'rb')
+# bytes = f.read(8)
+# print(bytes)
+# Adapted from:
+	# https://docs.python.org/3/library/stdtypes.html#int.from_bytes
+# x = int.from_bytes(bytes, byteorder = 'big')
+# print("Big endian decoded value: %d" % (x))
+
+# y = int.from_bytes(bytes, byteorder = 'little')
+# print("Little endian decoded value: %d" % (y))
+
+# print(struct.unpack('>II', bytes))
+
