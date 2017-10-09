e3829c7db376d86542e5223847185991ca09cfae
M.Vainauskas <mindelis1@gmail.com>
2017-10-09 13:40:07 +0100

    Saving of images from array now works. Added function to process and save images from range in array. Commented out for now. Added option for user to select the image. Huge speed of processing increase with buffered array

--- mnist-import-gz.py
+++ mnist-import-gz.py
@@ -17,54 +17,77 @@ def read_images_from_file(fPath):
 		print("Magic number: ", mNum)		
 		print("Image amount: ", imgNum)		
 		print("Row number: ", noRows)		
-		print("Column number: ", noCols)		
+		print("Column number: ", noCols)	
 
-		images = []
+		#Adapted from(For a massive speed of processing increase!)
+		#https://gist.github.com/ischlag/41d15424e7989b936c1609b53edd1390
+		#I needed to do this as my laptop was taking over 5 minutes to return results of image processing.
+		buffer = f.read(noRows * noCols * imgNum)
+		images = np.frombuffer(buffer, dtype=np.uint8).astype(np.float32)
+		images = images.reshape(imgNum, noRows, noCols, 1)
+
+		# images = []
 		
-		for i in range(imgNum):
-			rows = []
-			for r in range(noRows):
-				cols = []
-				for c in range(noCols):
-					cols.append(int.from_bytes(f.read(1), 'big'))
-				rows.append(cols)
-			images.append(rows)
+		# for i in range(imgNum):
+			# rows = []
+			# for r in range(noRows):
+				# cols = []
+				# for c in range(noCols):
+					# cols.append(int.from_bytes(f.read(1), 'big'))
+				# rows.append(cols)
+			# images.append(rows)
 		
 	return images
 	f.close()
 	
 train_images = read_images_from_file('data/train-images-idx3-ubyte.gz')
+
 #test_images = read_images_from_file('data/t10k-images-idx3-ubyte.gz')
 
 #Set selected image nuumber. Easier than looking where to change it when need to render different one.
-selectedImage = 3
+selectedImage = int(input("Which image to show?: "))
 
 def print_image(i):
 	for r in train_images[i]:
 		for col in r:
 			print('.' if col < 127 else '#', end='')
-		print()	
-		
-print_image(selectedImage)
+		print()		
 
+
+print("Selected image on screen was %s:" % selectedImage)
+
+#For image saving and rendering I adapted code from:
+#https://stackoverflow.com/questions/34324958/trying-to-create-noise-image-with-noise-numpy-and-image
 import PIL.Image as pil
-img = train_images[selectedImage]
-img = np.array(img)
-img = pil.fromarray(img)
-img = img.convert('RGB')
-img.show()
-img.save('Image_%s.png', selectedImage)
-# f.close()
-# f = gzip.open(dataPath, 'rb')
-# bytes = f.read(8)
-# print(bytes)
-# Adapted from:
-	# https://docs.python.org/3/library/stdtypes.html#int.from_bytes
-# x = int.from_bytes(bytes, byteorder = 'big')
-# print("Big endian decoded value: %d" % (x))
 
-# y = int.from_bytes(bytes, byteorder = 'little')
-# print("Little endian decoded value: %d" % (y))
+def save_image(i):
+	img = train_images[i]
+	img = np.asarray(img)
+	img = pil.fromarray(img, mode='RGBA')
+	img.show()
+	img.save('./images/Image_%d.png' % i)
+	
+#define function to print and save images in range
+def print_save_range(i, j):
+	for e in range(i, j):
+		print_image(e)
+		save_image(e)
+	
+#Calls to functions	
+print_image(selectedImage)
+save_image(selectedImage)
+# print("Enter range of images to show. Enter same number as first and last number to print single image!")
+# print("Numbers must ne in ascending order or equal")
+# startRange = int(input("Enter first number in range: "))
+# endRange= int(input("Enter last number in range: "))
+# endRange = endRange+1
 
-# print(struct.unpack('>II', bytes))
+# print_save_range(startRange, endRange)
 
+	
+	
+	
+	
+	
+	
+	
