1657eecaebec5e347b5bc80e77c10254440d8515
M.Vainauskas <mindelis1@gmail.com>
2017-10-18 15:40:01 +0100

    Added checks for valid/invalid inputs when entering range of images to process

--- mnist-import-gz.py
+++ mnist-import-gz.py
@@ -45,28 +45,22 @@ def read_images_from_file(fPath):
 	return images
 	f.close()
 	
-
+# Get the image and label sets from data folder
 label_set = read_labels_from_file('data/train-labels-idx1-ubyte.gz')
 image_set = read_images_from_file('data/train-images-idx3-ubyte.gz')
 
-#test_images = read_images_from_file('data/t10k-images-idx3-ubyte.gz')
-
-#Set selected image nuumber. Easier than looking where to change it when need to render different one.
-selectedImage = int(input("Which image to show?: "))
-
+# Print image on the console
 def print_image(i):
 	for r in image_set[i]:
 		for col in r:
 			print('.' if col < 127 else '#', end='')
 		print()		
 	print(label_set[i])
-print("Selected image on screen was %s:" % selectedImage)
 
 
 
 #For image saving and rendering I adapted code from:
 #https://stackoverflow.com/questions/34324958/trying-to-create-noise-image-with-noise-numpy-and-image
-
 def save_image(i):
 	img = image_set[i]
 	img = np.asarray(img, dtype=np.float32)
@@ -80,16 +74,26 @@ def print_save_range(i, j):
 		print_image(e)
 		save_image(e)
 	
+imCount = 60000
 #Calls to functions	
-print_image(selectedImage)
-save_image(selectedImage)
-# print("Enter range of images to show. Enter same number as first and last number to print single image!")
-# print("Numbers must ne in ascending order or equal")
-# startRange = int(input("Enter first number in range: "))
-# endRange= int(input("Enter last number in range: "))
-# endRange = endRange+1
+print()
+print("Enter range of images to show. Enter same number as first and last number to print single image!")
+print("Numbers must be in ascending order or equal")
+#check for valid/invalid input before assigning first/last numbers in range
+while 1:
+	startRange = int(input("Enter first number in range: "))
+	if startRange >= 0 and startRange < imCount:
+		break
+		
+while 1:
+	endRange= int(input("Enter last number in range: "))
+	if endRange >= startRange and endRange < imCount:
+		break
+		
+endRange = endRange+1
 
-# print_save_range(startRange, endRange)
+#run the rendering for given range of images
+print_save_range(startRange, endRange)
 
 	
 	
